15df060da233abab28840723b416f9ba3cd9dfda
Hunnuh18 <100071607+Hunnuh18@users.noreply.github.com>
2022-03-15 14:26:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,21 @@
     <title> My Home Page </title>
   </head>
   <body>
+    <!DOCTYPE html>
+<html>
+     <head>
+       <title>My Home Page</title>
+     </head>
+     <body>
+       <h1> Welcome to My Website! Hannah :)</h1>
+       <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+            <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
+       </ul>
+       </body>
+    
+</html>
   <h1> Welcome to My Website! I am Hannah! </h1>
   <ul>
     <li><a href="index.php" target="_blank"> HOME </a></li>
@@ -17,92 +32,4 @@
   </ul>
   </body>
 <html>
-    <head>
-        <title>My Top 10 Favorites</title>
-        <link rel="stylesheet" type="text/css" href="stylesheet.css">
-        <h1 style="rgba(253, 255, 247,.95)" >  My Top 10 Favorite Food </h1>
-    </head>
     
-    <body> 
-        <div id="wrapper">
-            <div id="box-one"> 
-                <p><b>Top 1</b></p>
-                <p><b>Adobo</b></p>
-                <img src="adobo.jpg" title="Adobo"
-               alt="Adobo" style="width:140px; height:190px" class="center" />
-                           <p style="color:rgba(241, 247, 226, .70)">Adobo or adobar (Spanish: marinade, sauce, or seasoning) is the immersion of cooked food in a stock (or sauce) composed variously of paprika, oregano, salt, garlic, and vinegar to preserve and enhance its flavor. </p>
-            </div>
-            
-            <div id="box-two">
-                <p><b>Top 2</b></p> 
-                <p><b>Bicol Express</b></p>
-                <img src="bikol.jpg" title="Bikol Express"
-               alt="Bikol Express" style="width:140px; height:190px" class="center"/>
-                           <p style="color:rgba(241, 247, 226, .70)">Bicol Express, known natively in Bikol as sinilihan (lit. 'spiced with chili'), is a popular Filipino dish which was popularized in the district of Malate, Manila but made in traditional Bicolano style. </p></div>
-            
-            <div id="box-three">
-                <p><b>Top 3</b></p>
-                <p><b>Buchi</b></p>
-                <img src="buchi.jpg" title="Buchi"
-               alt="Buchi" style="width:140px; height:190px" class="center"/>
-                          <p style="color:rgba(241, 247, 226, .70)">Jiandui (Chinese: 煎䭔; pinyin: jiānduī; Cantonese Yale: jīndēui; lit. 'fried dumpling', common misspelling 煎堆)[1] is a type of fried Chinese pastry made from glutinous rice flour.</p> </div>
-        
-            <div id="box-four">
-                <p><b>Top 4</b></p> 
-                <p><b>Halo-halo</b></p>
-                <img src="halo.jpg" title="Halo-halo"
-               alt="Halo-halo" style="width:140px; height:190px" class="center"/>
-                           <p style="color:rgba(241, 247, 226, .70)"> Halo-halo, officially spelled haluhalo, Tagalog for "mix-mix," is a popular cold dessert in the Philippines.</p>  </div>
-        
-            <div id="box-five">
-                <p><b>Top 5</b></p>
-                <p><b>Coffee Jelly</b></p>
-                <img src="jelly.jpg" title="Coffee Jelly"
-               alt="Coffee Jelly" style="width:140px; height:190px" class="center"/>
-                           <p style="color:rgba(241, 247, 226, .70)">Coffee jelly is a jelly dessert flavored with coffee and sugar.[1][2] Although once common in British and American cookbooks, it is now most common in Japan, where it can be found in most restaurants and convenience stores. </p> </div>
-            
-            <div id="box-six">
-                <p><b>Top 6</b></p> 
-                <p><b>Leche Flan</b></p>
-                <img src="leche.jpg" title="Leche Flan"
-               alt="Leche Flan" style="width:140px; height:190px" class="center"/>
-                         <p style="color:rgba(241, 247, 226, .70)">Crème caramel (French: [kʁɛm kaʁamɛl]), flan, caramel pudding or caramel custard is a custard dessert with a layer of clear caramel sauce.</p> </div>
-        
-            <div id="box-seven">
-                <p><b>Top 7</b></p> 
-                <p><b>Lechon</b></p>
-                <img src="lechon.jpg" title="Lechon"
-               alt="Lechon" style="width:140px; height:190px" class="center"/>
-                           <p style="color:rgba(241, 247, 226, .70)">Lechón (Spanish pronunciation: [leˈt͡ʃon]; from leche "milk" + -ón) is a pork dish in several regions of the world, most specifically in Spain and former Spanish colonial possessions throughout the world.</p> </div>
-            
-            <div id="box-eight">
-                <p><b>Top 8</b></p> 
-                <p><b>Pancake</b></p>
-                <img src="pancake.jpg" title="Pancake"
-               alt="Pancake" style="width:140px; height:190px" class="center"/>
-                          <p style="color:rgba(241, 247, 226, .70)">A pancake is a flat cake, prepared from a starch-based batter that may contain eggs, milk and butter and cooked on a hot surface such as a griddle or frying pan, often frying with oil or butter. It is a type of batter bread.</p> </div>
-            
-           <div id="box-nine">
-                <p><b>Top 9</b></p> 
-                <p><b>Sinigang</b></p>
-                <img src="sinigang.jpg" title="Sinigang"
-               alt="Sinigang" style="width:140px; height:190px" class="center"/>
-                          <p style="color:rgba(241, 247, 226, .70)">Sinigang is a Filipino soup or stew characterized by its sour and savoury taste. It is most often associated with tamarind (Filipino: sampalok), although it can use other sour fruits and leaves as the souring agent. </p> </div>
-        
-           <div id="box-ten">
-                <p><b>Top 10</b></p> 
-                <p><b>Buko Pandan</b></p>
-                <img src="pandan.jpg" title="Buko Pandan" 
-               alt="Buko Pandan" style="width:140px; height:190px" class="center"/>
-                          <p style="color:rgba(241, 247, 226, .70)">A popular variant of buko salad whose secondary ingredient are green gulaman (agar) cubes flavored with pandan leaf extracts.</p> </div>
-        
-        </body>
-
-
-
-
-
-
-
-
-</html>

e1e4d8cc2c15211a972f156a5183c02a29d6a666
Hunnuh18 <100071607+Hunnuh18@users.noreply.github.com>
2022-03-24 17:27:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
   <h1> Welcome to My Website! I am Hannah! </h1>
   <ul>
     <li><a href="index.php" target="_blank"> HOME </a></li>
-    <li><a href="/LT2/mywebsite.html" target= "_blank"> LEARNING TASK 2 </a> </li>
+    <li><a href="/LT2/Yongque.html" target= "_blank"> LEARNING TASK 2 </a> </li>
      <li><a href="/LT3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
-    <li><a href="/CLE1MT/Index.html" target= "_blank"> CLE1MT </a> </li>
+    <li><a href="/CLE1MT/index.html" target= "_blank"> CLE1MT </a> </li>
   </ul>
   </body>
 </html>

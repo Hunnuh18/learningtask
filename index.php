5455ce001f5670f307d4e9c7dbf224b37471a593
Hunnuh18 <100071607+Hunnuh18@users.noreply.github.com>
2022-03-24 17:10:23 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> Welcome to My Website! I am Hannah! </h1>
+  <ul>
+    <li><a href="index.php" target="_blank"> HOME </a></li>
+    <li><a href="/LT2/mywebsite.html" target= "_blank"> LEARNING TASK 2 </a> </li>
+     <li><a href="/LT3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
+    <li><a href="/CLE1MT/Index.html" target= "_blank"> CLE1MT </a> </li>
+  </ul>
+  </body>
+</html>

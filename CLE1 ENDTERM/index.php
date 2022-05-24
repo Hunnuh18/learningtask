89cbd179bcc0388fc065056b756093a7b6499832
Hunnuh18 <100071607+Hunnuh18@users.noreply.github.com>
2022-05-24 15:21:09 +0800

    Add files via upload

---
+++ CLE1 ENDTERM/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Portfolio</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#"> Portfolio</a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+    
+    
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div img src="hann.jpg" alt=""></div> 
+                <div class="text-1">Hi!</div>
+                <div class="text-2">I am Hannah G. Yongque</div>
+            </div>
+        </div>
+    </section>
+    
+    
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="hannah.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text"> Hello I am Hannah! <span class="typing-2"></span></div>
+            </div>
+                     <p>I am 19 Years Old. 
+                         Student in University of St. La Salle and I live in Brgy. Tanza Enrique B. Magalona</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact</h2>
+            <div class="contact-content">
+               <a href="#" class="fab fa-twitter"></a>
+  <a href="#" class="fab fa-instagram"></a>
+  <a href="#" class="fab fa-facebook-f"></a>
+  <a href="#" class="fab fa-telegram"></a>   
+                
+    </section>
+    <script src="script.js"></script>
+  <h2>My Portfolio</h2>
+        <div class="row">
+            <div class="column">
+                <div class="content">
+                    <img src="LT2.JPG"
+                        alt="" style="width:45%">
+                    <h3>
+                        <a href ="/activities/LT2/" target="_blank" rel="noopener noreferrer">Learning Task 2</a>
+                    </h3>
+                </div>
+            </div>
+            <div class="column">
+                <div class="content">
+                    <img src="LT3.JPG"
+                        alt="" style="width:45%">
+                    <h3>
+                        <a href ="/activities/LT3/" target="_blank" rel="noopener noreferrer">Learning Task 3</a>
+                    </h3>
+                </div>
+            </div>
+            <div class="column">
+                <div class="content">
+                    <img src="LT4.JPG"
+                        alt="" style="width:45%">
+                    <h3>
+                        <a href ="/activities/LT4/" target="_blank" rel="noopener noreferrer">LEARNING TASK 4</a>
+                    </h3>
+                </div>
+            </div>
+            <div class="column">
+                <div class="content">
+                    <img src="LT5.jpg"
+                        alt="" style="width:45%">
+                    <h3>
+                        <a href ="/activities/LT5/"target="_blank" rel="noopener noreferrer">LEARNING TASK 5</a>
+                    </h3>
+                    </div>
+                </div>
+                        <div class="center">
+                <div class="content">
+                    <img src="Midterm.jpg"
+                        alt="" style="width:45%">
+                    <h3>
+                          <a href ="/activities/CLE1MT/"target="_blank" rel="noopener noreferrer">CLE1MT</a>
+                    </h3>
+                    </div>
+                </div>
+        </div>
+    </body>
+</html>
